a6f4d9613d2f0a213ce8e24dee3811351dd8fd8e
Ade Rahmat Maulana <adermaulana15@gmail.com>
2024-03-18 17:35:36 +0800

    validatePIN

--- 3-validatePIN.js
+++ 3-validatePIN.js
@@ -2,11 +2,8 @@
 //https://www.codewars.com/kata/582cb0224e56e068d800003c/train/javascript
 
 function validatePIN(pin) {
-  //return true or false
-  if ((typeof pin == "number" && pin.length == 4) || pin.length == 6) {
-    return "true";
+    return /^(\d{4}|\d{6})$/.test(pin)
   }
-  return "false";
-}
 
-console.log(validatePIN(123456));
+  console.log(validatePIN("1234"))
+
