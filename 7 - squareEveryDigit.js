1813e2f4fb9bed23baa85dc403d21ffbde00cded
Ade Rahmat Maulana <adermaulana15@gmail.com>
2024-03-23 05:03:09 +0800

    7 - Square Every Digit

--- 7 - squareEveryDigit.js
+++ 7 - squareEveryDigit.js
@@ -2,5 +2,17 @@
 //https://www.codewars.com/kata/546e2562b03326a88e000020/train/javascript
 
 function squareDigits(num) {
-  return 0;
+  let numStr = num.toString();
+
+  let result = "";
+
+  for (let i = 0; i < numStr.length; i++) {
+    let digit = parseInt(numStr[i]);
+
+    result += digit * digit;
+  }
+
+  return parseInt(result);
 }
+
+console.log(squareDigits(9119));
