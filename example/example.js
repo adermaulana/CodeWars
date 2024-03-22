92312739ac9052aa355e61265a03f6b17fee9b49
Ade Rahmat Maulana <adermaulana15@gmail.com>
2024-03-23 04:55:01 +0800

    7 - Square Every Digit

--- example/example.js
+++ example/example.js
@@ -13,4 +13,22 @@
 //   return Array.from({ length: n }, (_, i) => i + 1);
 // }
 
-// console.log(count(10));
+// var fizzBuzz = function (n) {
+//   result = [];
+
+//   for (let i = 1; i <= n; i++) {
+//     if (i % 5 === 0 && i % 3 == 0) {
+//         result.push("FizzBuzz");
+//       }
+//       else if (i % 3 == 0) {
+//       result.push("Fizz");
+//     } else if (i % 5 == 0) {
+//       result.push("Buzz");
+//     } else {
+//       result.push(`${i}`);
+//     }
+//   }
+//   return result;
+// };
+
+// console.log(fizzBuzz(100));
